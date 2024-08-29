ee8131ba1c69afd907a22cedefc1c37e11512fd2
jackwinter <jackwinter.cn@gmail.com>
2024-08-30 00:20:39 +0800

    [feat] Update build script

--- package/build.sh
+++ package/build.sh
@@ -1,6 +1,20 @@
-cd ../build
+BUILD_DIR="../build"
+PACKAGE_OUTPUT_DIR="../output"
+MODEL_PARAM_DIR="../model_param"
+
+if [ ! -d "${BUILD_DIR}" ]; then
+    echo "mkdir ${BUILD_DIR}"
+    mkdir ${BUILD_DIR}
+fi
+
+if [ ! -d "${PACKAGE_OUTPUT_DIR}" ]; then
+    echo "mkdir ${PACKAGE_OUTPUT_DIR}"
+    mkdir ${PACKAGE_OUTPUT_DIR}
+fi
+
+cd ${BUILD_DIR}
 # rm ./* -rf
 cmake ..
 cmake --build .
-cp ./*_minist ../output
-cp ../model_param/* ../output/ -r
+cp ./*_minist ${PACKAGE_OUTPUT_DIR}
+cp ${MODEL_PARAM_DIR}/* ${PACKAGE_OUTPUT_DIR} -r
